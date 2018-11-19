51b3ab075a189cb20f3a8e522d309a91b5598aaf
root <root@localhost.localdomain>
2018-11-19 09:03:33 -0500

    0.2.0 - Sh code

---
+++ sh/app/notepad.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+### yum install epel-release yum-plugin-copr
+
+### wget https://copr.fedorainfracloud.org/coprs/ngompa/snapcore-el7/repo/epel-7/ngompa-snapcore-el7-epel-7.repo -O /etc/yum.repos.d/snapd.repo
+
+### yum -y install snapd
+
+### systemctl enable --now snapd.socket
+
+### ln -s /var/lib/snapd/snap /snap
+
+
+
+snap install notepad-plus-plus
---
+++ sh/app/wine.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+# INSTALL
+#DOC# https://tecadmin.net/steps-install-wine-centos-rhel-fedora-systems/
+
+### yum install gcc flex bison
+### yum clean all
+### yum update
+### yum groupinstall 'Development Tools'
+### yum install libX11-devel freetype-devel zlib-devel libxcb-devel \ 
+###      libxslt-devel libgcrypt-devel libxml2-devel gnutls-devel \
+###      libpng-devel libjpeg-turbo-devel libtiff-devel gstreamer-devel \ 
+###      dbus-devel fontconfig-devel
+
+
+### wget http://dl.winehq.org/wine/source/3.0/wine-3.0.tar.xz
+### tar -Jxf wine-3.0.tar.xz
+### cd wine-3.0
+### ./configure --enable-win64 --without-x --without-freetype
+### make
+### make install
+
+
+
+
+
+
+
+
