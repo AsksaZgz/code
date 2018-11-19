f29c4c38592775daa5f3885877c4e83b03d3e082
jesus.bernad <jesus.bernad@clearone.com>
2018-11-19 16:41:54 +0100

    0.3.1 remote.sh

--- sh/collaborate-space/remote.sh
+++ sh/collaborate-space/remote.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 _REMOTE_CREATE_TARBALL='tar czf COLLABORATE-Space-1.0.tar.gz COLLABORATE-Space'
+_SPEC=COLLABORATE-Space.spec
 
 cd /rpmSources
 
 $_REMOTE_CREATE_TARBALL
+
+cd /root/git/code/sh/collaborate-space/spec
+cp $_SPEC /rpmSpecs
+
+# BUILD RPM
+cd ~/rpmbuild
+rpmbuild -ba SPECS/$_SPEC
