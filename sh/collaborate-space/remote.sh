581d6723a62e854c0f0c4233ee8be48babfa711d
jesus.bernad <jesus.bernad@clearone.com>
2018-11-19 16:08:42 +0100

    0.2.0 COLLABORATE Space sh code

---
+++ sh/collaborate-space/remote.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+_REMOTE_CREATE_TARBALL='tar czf COLLABORATE-Space-1.0.tar.gz COLLABORATE-Space'
+
+cd /root/rpmbuild
+
+$_REMOTE_CREATE_TARBALL
---
+++ sh/collaborate-space/rpm.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+_REMOTE_PASS=clearone!
+_REMOTE_SERVER=root@192.168.0.17
+
+
+
+
+
+function send()
+{
+    _sendTarget=$1
+    _sendFile=$2
+
+    sshpass -p $_REMOTE_PASS scp $_sendFile root@192.168.0.17:/$_sendTarget
+
+
+}
+
+function sendSource()
+{
+    _sendSourcesFile=$1
+    send rpmSources/COLLABORATE-Space $_sendSourcesFile
+}
+
+
+
+function remoteExec()
+{
+    _remoteExec=$1
+
+    sshpass -p $_REMOTE_PASS ssh $_REMOTE_SERVER "$_remoteExec"
+
+}
+
+function main()
+{
+    sendSource /root/git/Admin/war/admin.war
+    remoteExec '/root/sh/code/collaborate-space/remote.sh'
+
+}
