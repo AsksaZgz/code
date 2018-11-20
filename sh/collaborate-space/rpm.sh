d201b61fafdd79792e79d0a4c2165ae8b1ace670
jesus.bernad <jesus.bernad@clearone.com>
2018-11-20 10:01:28 +0100

    0.5.0 Test Action

--- sh/collaborate-space/rpm.sh
+++ sh/collaborate-space/rpm.sh
@@ -3,7 +3,10 @@
 _REMOTE_PASS=clearone!
 _REMOTE_SERVER=root@192.168.0.17
 
+_TEST_PASS=clearone!
+_TEST_SERVER=root@192.168.0.37
 
+_RPM=COLLABORATE-Space-1-0.noarch.rpm
 
 
 
@@ -12,7 +15,7 @@ function send()
     _sendTarget=$1
     _sendFile=$2
 
-    sshpass -p $_REMOTE_PASS scp $_sendFile root@192.168.0.17:/$_sendTarget
+    sshpass -p $_REMOTE_PASS scp $_sendFile $_REMOTE_SERVER:/$_sendTarget
 
 
 }
@@ -21,6 +24,16 @@ function sendSource()
 {
     _sendSourcesFile=$1
     send rpmSources/COLLABORATE-Space-1 $_sendSourcesFile
+}
+
+
+function testSend()
+{
+    _testSendFile=$1
+    _testSendTarget=tmp
+
+    sshpass -p $_TEST_PASS scp $_testSendFile $_TEST_SERVER:/$_testSendTarget
+
 }
 
 
@@ -33,11 +46,22 @@ function remoteExec()
 
 }
 
+function testExec()
+{
+    _testExec=$1
+
+    sshpass -p $_TEST_PASS ssh $_TEST_SERVER "$_remoteExec"
+
+}
+
 function main()
 {
     remoteExec '/root/git/code/sh/collaborate-space/remote.sh before-build'
     sendSource /root/git/Admin/war/admin.war
     remoteExec '/root/git/code/sh/collaborate-space/remote.sh build'
+
+    testSend '/rpmRpms/noarch/$_RPM'
+    testExec '/tmp/rpm -ivh /tmp/$_RPM'
 }
 
 main
