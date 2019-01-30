097e0b906f980637787de71ad1e89bcbf4808ee6
jesus.bernad <jesus.bernad@clearone.com>
2019-01-30 10:39:12 +0100

    Rename

--- sh/dev/angular/action.sh
+++ sh/dev/angular/action.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 
-_remoteAction=$1
+_action=$1
 _FOLDER_NAME=${PWD##*/}
 _PARENT_NAME="$(basename "$(dirname "$PWD")")"
 
 
-if [[ $_remoteAction = 'd' ]]
+if [[ $_action = 'd' ]]
 then
     echo DEVELOPING
 fi
 
 
-if [[ $_remoteAction = 'module' ]]
+if [[ $_action = 'module' ]]
 then
     # Phone Billing - 1812051158
     # https://github.com/angular/angular-cli/wiki/generate-module
     ng generate module --spec=false --flat=true $2
 fi
 
-if [[ $_remoteAction = 'page' ]]
+if [[ $_action = 'page' ]]
 then
     _FOLDER_NAME=${PWD##*/}
     _pageModule=$_FOLDER_NAME
@@ -37,7 +37,7 @@ fi
 
 
 
-if [[ $_remoteAction = 'widget' ]]
+if [[ $_action = 'widget' ]]
 then
     # Phone Billing - 1812051158
     # https://github.com/angular/angular-cli/wiki/generate-component
@@ -60,7 +60,7 @@ fi
 
 
 
-if [[ $_remoteAction = 'bus' ]]
+if [[ $_action = 'bus' ]]
 then
 
     ng generate class "$_FOLDER_NAME"-class --spec=false
@@ -68,4 +68,21 @@ then
     ng generate class "$_FOLDER_NAME"-load --spec=false
     ng generate class "$_FOLDER_NAME"-setup --spec=false
 
+fi
+
+
+#1901081349 - Update angular 6.1.7
+if [[ $_action = 'update' ]]
+then
+#    echo UPDATE - 3
+#    # Phone Billing - 1812051158
+#    # https://github.com/angular/angular-cli/wiki/generate-module
+#    npm install @angular/cli@6.2.2 --save
+##    ng update @angular/cli --to=6.2.2
+##    ng update @angular/cli@6.2.2
+##    npm install @angular/core6.1.7 --save
+##    ng update @angular/core --to=6.1.7
+##    ng update @angular/core@6.1.7
+#    npm install -g rxjs-tslint
+#    rxjs-5-to-6-migrate -p src/tsconfig.app.json
 fi
